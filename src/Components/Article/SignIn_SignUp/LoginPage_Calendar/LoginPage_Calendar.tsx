fa52b942a6a1ee043ada9ff4b6704f177b225e4b
lyd1040 <skswngussla@naver.com>
2023-08-03 18:46:25 +0900

    회원가입 구현

--- src/Components/Article/SignIn_SignUp/LoginPage_Calendar/LoginPage_Calendar.tsx
+++ src/Components/Article/SignIn_SignUp/LoginPage_Calendar/LoginPage_Calendar.tsx
@@ -1,16 +1,16 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../../css/LoginPage_Calendar.css'
 
 function LoginPage_Calendar() {
     //로그인 달력 요일
-    const [LoginDays,setLoginDays] = useState<JSX.Element[]>([]) 
-    const LoginCalendarDays =()=>{
-        const days:string[] = ['일','월','화','수','목','금','토']
-        const LoginDaysElement:JSX.Element[] = [];
-        
-        for(let x=0; x<days.length; x++){
+    const [LoginDays, setLoginDays] = useState<JSX.Element[]>([])
+    const LoginCalendarDays = () => {
+        const days: string[] = ['일', '월', '화', '수', '목', '금', '토']
+        const LoginDaysElement: JSX.Element[] = [];
+
+        for (let x = 0; x < days.length; x++) {
             LoginDaysElement.push(
-                <li key={'LoginDaysElement'+x}>{days[x]}</li>
+                <li key={'LoginDaysElement' + x}><span>{days[x]}</span></li>
             )
         }
 
@@ -18,20 +18,20 @@ function LoginPage_Calendar() {
     }
 
     //오늘 년도, 월, 일
-    const [LoginYM,setLoginYM] = useState<JSX.Element[]>([])  
-    const LoginTodayYM=()=>{
-        const LoginYMElement:JSX.Element[] =[];
+    const [LoginYM, setLoginYM] = useState<JSX.Element[]>([])
+    const LoginTodayYM = () => {
+        const LoginYMElement: JSX.Element[] = [];
         const todaynewDate = new Date();
         const todayYear = todaynewDate.getFullYear();
-        const todayMonth = todaynewDate.getMonth()+1;
-        const todayarr:number[] = [todayYear,todayMonth];
-        let YMtext:string = '년';
+        const todayMonth = todaynewDate.getMonth() + 1;
+        const todayarr: number[] = [todayYear, todayMonth];
+        let YMtext: string = '년';
 
-        for(let x=0; x<todayarr.length; x++){
-            if(x===0){YMtext='년'}
-            else{YMtext='월'}
+        for (let x = 0; x < todayarr.length; x++) {
+            if (x === 0) { YMtext = '년' }
+            else { YMtext = '월' }
             LoginYMElement.push(
-                <div key={'LoginYM'+x}>{todayarr[x]}{YMtext}</div>
+                <div key={'LoginYM' + x}>{todayarr[x]}{YMtext}</div>
             )
         }
 
@@ -39,61 +39,60 @@ function LoginPage_Calendar() {
     }
 
     //로그인 달력
-    const [LoginCalendar,setLoginCalendar] = useState<JSX.Element[]>([]);
-    const LoginTodayCalendar=()=>{
-        const LoginCalendarLis:JSX.Element[][]=[];
-        const LoginCalendarUls:JSX.Element[]=[];
-        const Today:Date = new Date();
-        const prevLastWeekLastDate:number = new Date(Today.getFullYear(),Today.getMonth(),0).getDate();
-        const startWeekstartDay:number = new Date(`${Today.getFullYear()}-${Today.getMonth()+1}-1`).getDay();
-        const lastWeeklastDate:number = new Date(Today.getFullYear(),Today.getMonth()+1,0).getDate()
-        const Weeks:number = LoginWeeks(Today.getFullYear(), Today.getMonth()); //ul태그
-        let prev_count_days:number=startWeekstartDay-1;
-        let count_days:number = 0;
-        let Lowten:string='';
-        let next_count_days:number = 0;
-        
+    const [LoginCalendar, setLoginCalendar] = useState<JSX.Element[]>([]);
+    const LoginTodayCalendar = () => {
+        const LoginCalendarLis: JSX.Element[][] = [];
+        const LoginCalendarUls: JSX.Element[] = [];
+        const Today: Date = new Date();
+        const prevLastWeekLastDate: number = new Date(Today.getFullYear(), Today.getMonth(), 0).getDate();
+        const startWeekstartDay: number = new Date(`${Today.getFullYear()}-${Today.getMonth() + 1}-1`).getDay();
+        const lastWeeklastDate: number = new Date(Today.getFullYear(), Today.getMonth() + 1, 0).getDate()
+        const Weeks: number = LoginWeeks(Today.getFullYear(), Today.getMonth()); //ul태그
+        let prev_count_days: number = startWeekstartDay - 1;
+        let count_days: number = 0;
+        let Lowten: string = '';
+        let next_count_days: number = 0;
+
         //lis 2차원배열 초기화
-        for(let x=0; x<Weeks; x++){
-            LoginCalendarLis[x]=[];
+        for (let x = 0; x < Weeks; x++) {
+            LoginCalendarLis[x] = [];
         }
 
         //달력 날짜 그리기
-        for(let x=0; x<Weeks; x++){
-            for(let y=(x*7); y<(x*7)+7; y++){
-                if(y>=startWeekstartDay && y<(startWeekstartDay+lastWeeklastDate)){
+        for (let x = 0; x < Weeks; x++) {
+            for (let y = (x * 7); y < (x * 7) + 7; y++) {
+                if (y >= startWeekstartDay && y < (startWeekstartDay + lastWeeklastDate)) {
                     count_days++;
 
                     //10 이하일때 앞에 0 붙여주기
-                    if(count_days<10){Lowten='0'}
-                    else{Lowten=''}
+                    if (count_days < 10) { Lowten = '0' }
+                    else { Lowten = '' }
 
                     LoginCalendarLis[x].push(
-                        <li key={'LoginCalendarLis'+y}>{Lowten}{count_days}</li>
+                        <li key={'LoginCalendarLis' + y}><span>{Lowten}{count_days}</span></li>
                     )
-                    console.log(count_days);
-                }else{
-                    if(x===0){
+                } else {
+                    if (x === 0) {
                         LoginCalendarLis[x].push(
-                            <li key={'LoginCalendarLis'+y} className='prev_next'>{prevLastWeekLastDate - prev_count_days}</li>
+                            <li key={'LoginCalendarLis' + y} className='prev_next'><span>{prevLastWeekLastDate - prev_count_days}</span></li>
                         )
                         prev_count_days--;
-                    }else{
+                    } else {
                         next_count_days++;
 
                         //10 이하일때 앞에 0 붙여주기
-                        if(next_count_days<10){Lowten='0';}
-                        else{Lowten='';}
+                        if (next_count_days < 10) { Lowten = '0'; }
+                        else { Lowten = ''; }
 
                         LoginCalendarLis[x].push(
-                            <li key={'LoginCalendarLis'+y} className='prev_next'>{Lowten}{next_count_days}</li>
+                            <li key={'LoginCalendarLis' + y} className='prev_next'><span>{Lowten}{next_count_days}</span></li>
                         )
                     }
                 }
             }
         }
 
-        for(let x=0; x<Weeks; x++){
+        for (let x = 0; x < Weeks; x++) {
             LoginCalendarUls.push(
                 <ul key={`LoginCalendarUls${x}`}>
                     {LoginCalendarLis[x]}
@@ -104,27 +103,29 @@ function LoginPage_Calendar() {
     }
 
     //해당 월에 몇주인지 구하기
-    const LoginWeeks=(Year:number, Month:number):number=>{
+    const LoginWeeks = (Year: number, Month: number): number => {
 
-        const start_day:number = new Date(`${Year}-${Month+1}-1`).getDay();
-        const end_date:number = new Date(Year,Month+1,0).getDate();
-        
-        return Math.ceil((start_day+end_date)/7);
+        const start_day: number = new Date(`${Year}-${Month + 1}-1`).getDay();
+        const end_date: number = new Date(Year, Month + 1, 0).getDate();
+
+        return Math.ceil((start_day + end_date) / 7);
     }
 
     //마운트시 최초 실행
-    useEffect(()=>{
+    useEffect(() => {
         LoginTodayYM();
         LoginCalendarDays();
         LoginTodayCalendar();
-    },[])
+    }, [])
     return (
         <div id="LoginPage_Calendar_wrap" className='LoginPage_Calendar_wrap'>
-            
+
             <div>
                 <div id='LoginTodayYM' className='LoginTodayYM'>
-                    {LoginYM}
-                    <button>btn</button>
+                    <div>
+                        {LoginYM}
+                    </div>
+
                 </div>
                 <ul className='LoginCalendarDays' id='LoginCalendarDays'>
                     {LoginDays}
@@ -132,9 +133,12 @@ function LoginPage_Calendar() {
                 <div id='LoginCalendar' className='LoginCalendar'>
                     {LoginCalendar}
                 </div>
+                <div id='show_ClockBtnWrap' className='show_ClockBtnWrap'>
+                    <button className='showClockBtn'>시계</button>
+                </div>
             </div>
 
-            
+
         </div>
     )
 }
--- src/Components/Article/SignIn_SignUp/SignUp/SignUp.tsx
+++ src/Components/Article/SignIn_SignUp/SignUp/SignUp.tsx
@@ -1,9 +1,247 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../../../../css/SignUp.css'
+import { createUserWithEmailAndPassword, sendEmailVerification, Auth, UserCredential, User } from 'firebase/auth';
+import { auth } from '../../../../firebase';
+import { db } from '../../../../firebase'
+import { ref, get, set } from 'firebase/database';
+
+type FunctionType = () => void;
+
 function SignUp() {
+
+    //input 태그 그리기
+    const [SignUpTags, setSignUpTags] = useState<JSX.Element[]>([])
+    const SignupTags_draw = async (): Promise<void> => {
+        let label_name: string[] = ['이메일', '비밀번호', '비밀번호 확인', '이름', '생년월일'];
+        let label_input_link: string[] = ['SignUpuserID', 'SignUpuserPW', 'SignUpuserPWCheck', 'SignUpusername', 'SignUpuserbirth'];
+        let inputTags_type: string[] = ['email', 'password', 'password', 'text', 'text'];
+        let validationfilter: (null | FunctionType)[] = [emailValidation, passwordValidation, checkPwValidation, null, null];
+        let Tags_draw: JSX.Element[] = [];
+        for (let x = 0; x < label_name.length; x++) {
+            Tags_draw.push(
+                <div key={`SignUpFormTagsDraw` + x}>
+                    <label htmlFor={label_input_link[x]} className={label_input_link[x] + x + 'label'}>{label_name[x]}</label>
+                    <input type={inputTags_type[x]} id={label_input_link[x]} className={label_input_link[x]} required
+                        onFocus={(event) => {
+                            let SelectLabel: HTMLElement = document.querySelector(`.${label_input_link[x] + x}label`) as HTMLElement;
+                            SelectLabel.classList.add('active');
+
+                            event.target.addEventListener('focusout', () => {
+                                if (event.target.value === '') {
+                                    SelectLabel.classList.remove('active');
+                                }
+                            })
+                        }}
+                        onKeyUp={() => {
+                            if (validationfilter[x] !== null) { validationfilter[x]?.(); }
+                        }}
+                    />
+                </div>
+            )
+        }
+        setSignUpTags(Tags_draw);
+    }
+
+    //button 태그 그리기
+    const [SignUpbtnTags, setSignUpbtnTags] = useState<JSX.Element[]>([])
+    const SignUpBtnTags_draw = async (): Promise<void> => {
+        let BtnName: string[] = ['이전', '회원가입']
+        let BtnFunction: FunctionType[] = [showSignIn, AllValidation]
+        let BtnTags_draw: JSX.Element[] = [];
+        for (let x = 0; x < BtnName.length; x++) {
+            BtnTags_draw.push(
+                <button type="button" key={`SignUpbtnTags` + x} onClick={() => { BtnFunction[x]() }}>{BtnName[x]}</button>
+            )
+        }
+        setSignUpbtnTags(BtnTags_draw);
+    }
+
+    //이메일 입력시
+    let [EmailText, setEmailText] = useState<string>('');
+    const emailValidation = (): void | boolean => {
+        let SignUpuserID: HTMLInputElement = document.querySelector('.SignUpuserID') as HTMLInputElement
+        const emailValidation: RegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
+
+        if (SignUpuserID) {
+            setEmailText(SignUpuserID.value);
+            if (emailValidation.test(SignUpuserID.value)) {
+                //이메일 성공
+                return true;
+            } else { return false; }
+        }
+    }
+
+    //비밀번호 입력시
+    let [PassWordText, setPassWordText] = useState<string>('');
+    const passwordValidation = (): void | boolean => {
+        let SignUpuserPW: HTMLInputElement = document.querySelector('.SignUpuserPW') as HTMLInputElement;
+        var PasswordValidation: RegExp = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@!#$%^&*])[A-Za-z\d@!#$%^&*]{8,15}$/;
+
+        if (SignUpuserPW) {
+            setPassWordText(SignUpuserPW.value);
+            if (PasswordValidation.test(SignUpuserPW.value)) {
+                //비밀번호 성공
+                return true;
+            } else { return false; }
+        }
+    }
+
+    //비밀번호 확인 입력시
+    let [CheckPassWordText, setCheckPassWordText] = useState<string>('');
+    const checkPwValidation = (): void | Boolean => {
+        let SignUpuserPW: HTMLInputElement = document.querySelector('.SignUpuserPW') as HTMLInputElement;
+        let SignUpuserPWCheck: HTMLInputElement = document.querySelector('.SignUpuserPWCheck') as HTMLInputElement
+
+        if (SignUpuserPW && SignUpuserPWCheck) {
+            setCheckPassWordText(SignUpuserPWCheck.value);
+            if (SignUpuserPW.value === SignUpuserPWCheck.value) {
+                return true;
+            } else { return false; }
+        }
+    }
+
+    //이름 유효성 검사
+    let [UserNameText, setUserNameText] = useState<string>('');
+    const checkUserName = (): void | Boolean => {
+        let SignUpusername: HTMLInputElement = document.querySelector('.SignUpusername') as HTMLInputElement;
+        const nameValidation: RegExp = /^[가-힣]{2,10}$/;
+        if (SignUpusername) {
+            setUserNameText(SignUpusername.value);
+            if (nameValidation.test(SignUpusername.value)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    //생일 유효성 검사
+    let [UserBirthDayText, setUserBirthDayText] = useState<string>('');
+    const birthdayValidation = (): void | Boolean => {
+        const SignUpuserbirth: HTMLInputElement = document.querySelector('.SignUpuserbirth') as HTMLInputElement
+        const BirthDayCheck: RegExp = /^\d{8}$/;
+
+
+        if (SignUpuserbirth) {
+            setUserBirthDayText(SignUpuserbirth.value);
+            if (BirthDayCheck.test(SignUpuserbirth.value)) {
+
+                // 생년, 월, 일 추출
+                const birthDayYear: number = parseInt(SignUpuserbirth.value.slice(0, 4), 10);
+                const birthDayMonth: number = parseInt(SignUpuserbirth.value.slice(4, 6), 10);
+                const birthDayDay: number = parseInt(SignUpuserbirth.value.slice(6, 8), 10);
+
+                // 생년, 월, 일의 범위 확인
+                const currentYear: number = new Date().getFullYear();
+                const Date31: RegExp = /(1|3|5|7|8|10|12)/;
+                const Date30: RegExp = /(4|6|9|11)/;
+
+                if (birthDayYear < 1900 || birthDayDay > currentYear) {
+                    return false;
+                } else if (birthDayMonth < 1 || birthDayMonth > 12) {
+                    return false;
+                } else if (birthDayMonth === 2) {
+                    //윤년 확인
+                    if (birthDayYear % 4 !== 0 && (birthDayDay < 1 || birthDayDay > 28)) {
+                        return false;
+                    } else if (birthDayYear % 4 === 0 && (birthDayDay < 1 || birthDayDay > 29)) {
+                        return false;
+                    }
+                } else if (Date31.test(birthDayMonth.toString()) && (birthDayDay < 1 || birthDayDay > 31)) {
+                    return false
+                } else if (Date30.test(birthDayMonth.toString()) && (birthDayDay < 1 || birthDayDay > 30)) {
+                    return false
+                } else {
+                    return true;
+                }
+            }
+            else {
+                return false
+            }
+        }
+
+
+    }
+
+    //회원가입 클릭시
+    const AllValidation = async (): Promise<void | boolean> => {
+        let SignUpuserID: HTMLInputElement = document.querySelector('.SignUpuserID') as HTMLInputElement
+        let SignUpuserPW: HTMLInputElement = document.querySelector('.SignUpuserPW') as HTMLInputElement
+        let SignUpuserPWCheck: HTMLInputElement = document.querySelector('.SignUpuserPWCheck') as HTMLInputElement
+        let SignUpusername: HTMLInputElement = document.querySelector('.SignUpusername') as HTMLInputElement
+        let SignUpuserbirth: HTMLInputElement = document.querySelector('.SignUpuserbirth') as HTMLInputElement
+
+
+        if (SignUpuserID && SignUpuserPW && SignUpuserPWCheck && SignUpusername && SignUpuserbirth) {
+            if (emailValidation() === false) {
+                console.log('emailValidation')
+                return false;
+            }
+            if (passwordValidation() === false) {
+                console.log('passwordValidation')
+                return false;
+            }
+            if (checkPwValidation() === false) {
+                console.log('checkPwValidation')
+                return false;
+            }
+            if (checkUserName() === false) {
+                console.log('checkUserName')
+                return false;
+            }
+            if (birthdayValidation() === false) {
+                console.log('birthdayValidation')
+                return false;
+            }
+            console.log('success');
+
+            try {
+                await createUserWithEmailAndPassword(auth, SignUpuserID.value, SignUpuserPW.value);
+
+                // 사용자가 인증되어 있는지 확인 후 이메일 검증을 보냅니다.
+                const user: User | null = auth.currentUser;
+                if (user) {
+                    await sendEmailVerification(user);
+                    alert('이메일 인증메일을 보냈습니다.');
+                } else {
+                    // 사용자가 인증되어 있지 않은 경우 처리
+                    console.error('사용자가 인증되어 있지 않습니다.');
+                }
+            } catch (error: any) {
+                console.error('회원가입 도중 오류 발생:', error.message);
+            }
+
+        }
+    }
+
+    //이전버튼 클릭시
+    const showSignIn = (): void => {
+        console.log('showSignIn')
+    }
+
+    const RootFunction = async () => {
+        await SignupTags_draw();
+        await SignUpBtnTags_draw();
+    }
+    //마운트시 실행
+    useEffect(() => {
+        RootFunction()
+    }, [EmailText])
+
     return (
         <div id="SignUp" className="SignUp">
             <h2>회원가입</h2>
+            <form action="" method="post" name="">
+                <div>
+                    {SignUpTags}
+                    <p className="erroralram"></p>
+                    <div id="SignUpBtnWrap" className="SignUpBtnWrap">
+                        {SignUpbtnTags}
+                    </div>
+
+                </div>
+            </form>
+
         </div>
     )
 }
